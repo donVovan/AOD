474c0fa2ba84b3d59f67e3d4f492a51a4cbc96a2
donVovan <vladimir.a.blinov@gmail.com>
2022-11-11 14:57:48 +0300

    Подключил меню Бургер

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,19 +1,6 @@
-/* $('.wrapper').addClass('loaded');
+"use strict"
 
-$('.icon-menu').click(function(event) {
-    $(this).toggleClass('active');
-    $('.menu__body').toggleClass('active');
-    $('body').toggleClass('lock');
-});
-*/
-document.querySelector('.wrapper').classList.add('loaded');
-
-document.querySelector('.icon-menu').click(function(event) {
-    document.querySelector(this).classList.toggle('active');
-    document.querySelector('.menu__body').classList.toggle('active');
-    document.querySelector('body').classList.toggle('lock');
-});
-
+// IBG=======================================================================================
 function ibg() {
     const ibg=document.querySelectorAll(".ibg");
     for (let i = 0; i < ibg.length; i++) {
@@ -23,3 +10,88 @@ function ibg() {
     }
 }
 ibg();
+
+
+// Бургер ===================================================================================
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    const menuArrows = document.querySelectorAll('.menu__arrow');
+    if (menuArrows.length>0) {
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function(e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+
+} else {
+    document.body.classList.add('_pc');
+}
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if(iconMenu) {
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+// Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick)
+    });
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.prevenDefault();
+        }
+    }
+}
